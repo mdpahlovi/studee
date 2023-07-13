8576ecb3531264b2643e6eedbe05b7cf148a650a
MD Pahlovi <mdpahlovi07@gmail.com>
2023-07-13 20:42:18 +0600

    [Done] Project Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@material-tailwind/react";
+
+function App() {
+    return (
+        <>
+            <Button>Button</Button>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </>
+    );
+}
+
+export default App;
